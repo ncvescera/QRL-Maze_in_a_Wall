792b80f77585013e9d11aeca4d1515440a1ebbaa
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-01-27 18:14:10 +0100

    Risolti TODO
    Utilizzata la funzione is_wall_colliding.
    Eliminati commenti di codice inutile.

--- mazeinawall/main.py
+++ mazeinawall/main.py
@@ -17,6 +17,7 @@ if __name__ == '__main__':
                           [1, 1, 1, 1, 1, 1, 1, 1, 0]])
     '''
 
+    # TODO: migliorare output menu
     command = input("***********************\nGridWorld Demo\n***********************\nTraining (1)  \nExecute step-by step (2)\nExit (0)")
     if command == '1':
         print("Training\n")
