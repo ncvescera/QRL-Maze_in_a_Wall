79cb1a79b2412733fccc4c098de942c20647cca2
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-01-27 19:18:23 +0100

    Risolti TODO
    Migliorato menu iniziale.

--- mazeinawall/main.py
+++ mazeinawall/main.py
@@ -1,29 +1,52 @@
 from GridWorld import GridWorld
 from QLearning import QLearning
+import numpy as np
+from os import system
 
+def main():
+    menu()
 
-if __name__ == '__main__':
+    """
+        grid = np.array([[0, 1, 1, 0, 0, 0, 0, 0, 0],
+                              [0, 1, 0, 0, 0, 0, 0, 0, 0],
+                              [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                              [1, 1, 0, 0, 1, 1, 0, 1, 0],
+                              [1, 1, 0, 0, 1, 0, 0, 0, 0],
+                              [1, 1, 1, 1, 1, 0, 0, 0, 0],
+                              [1, 1, 0, 0, 0, 0, 0, 0, 0],
+                              [1, 1, 1, 1, 1, 1, 1, 0, 0],
+                              [1, 1, 1, 1, 1, 1, 1, 1, 0]])
+        env = GridWorld(grid=grid)
+    """
+
     env = GridWorld(10, 10)
     QL = QLearning(env)
 
-    '''self.grid = np.array([[0, 1, 1, 0, 0, 0, 0, 0, 0],
-                          [0, 1, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                          [1, 1, 0, 0, 1, 1, 0, 1, 0],
-                          [1, 1, 0, 0, 1, 0, 0, 0, 0],
-                          [1, 1, 1, 1, 1, 0, 0, 0, 0],
-                          [1, 1, 0, 0, 0, 0, 0, 0, 0],
-                          [1, 1, 1, 1, 1, 1, 1, 0, 0],
-                          [1, 1, 1, 1, 1, 1, 1, 1, 0]])
-    '''
+    command = input(": ")
+    system("clear")
 
-    # TODO: migliorare output menu
-    command = input("***********************\nGridWorld Demo\n***********************\nTraining (1)  \nExecute step-by step (2)\nExit (0)")
     if command == '1':
         print("Training\n")
         QL.training(epochs=50000, steps=200, ALPHA=0.1, GAMMA=1.0, EPS=1.0, plot=True)
     elif command == '2':
         print("Execute\n")
         QL.execute()
+
     else:
         print('End\n')
+
+
+def menu():
+    system("clear")
+
+    print("***********************")
+    print("GridWorld Demo")
+    print("***********************")
+    print("")
+    print("\tTraining (1)")
+    print("\tExecute step-by step (2)")
+    print("\tExit (0)")
+
+
+if __name__ == '__main__':
+    main()
