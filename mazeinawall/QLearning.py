77497b70bb11f8c9d48dda1cb93d31d7204530a7
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-01-28 09:18:24 +0100

    Commentato QLearning
    Aggiunti commenti e typing al file QLearning.

--- mazeinawall/QLearning.py
+++ mazeinawall/QLearning.py
@@ -1,4 +1,6 @@
 from os import lseek
+from typing import Dict, Tuple, Any, Union
+
 import numpy as np
 import matplotlib.pyplot as plt
 from time import sleep
@@ -9,87 +11,113 @@ class QLearning(object):
     def __init__(self, env):
         self.env = env
 
+    def maxAction(self, Q, state, actions: list[str]) -> str:
+        """
+        Trova l'azione che ha valore massimo per uno stato.
 
-    def maxAction(self,Q, state, actions):
-        values = np.array([Q[state,a] for a in actions])
+        :param Q: matrice Q
+        :param state: stato
+        :param actions: possibili azioni
+        :return:
+        """
+
+        values = np.array([Q[state, a] for a in actions])
+        action = np.argmax(values)  # TODO: lo fa due volte, ha senso ??
         action = np.argmax(values)
-        action = np.argmax(values)
+
         return actions[action]
 
-    #print Matrix Q(state, action)
-    def printQ(self,Q):
-        for i in range(self.env.m):
-            for j in range(self.env.n):
-                print("(",i,",",j,")")
-                for action in self.env.possibleActions:
-                    print(Q[i*j,action]," ")
-                print(" -")
-            print("\n")
+    def saveQ(self, Q, fileName):
+        """
+        Salva la matrice Q in un file.
 
-    #save Matrix Q(state, action) in fileName
-    def saveQ(self,Q,fileName):
-        # numStates = self.env.m*self.env.n
+        :param Q: matrice Q da salvare
+        :param fileName: nome del file su cui salvare la matrice
+        """
+
         numStates = len(self.env.stateSpace)
-        Qmatrix = np.zeros( (numStates, 4))
-        x=0
+        Qmatrix = np.zeros((numStates, 4))
+
+        x = 0
         for state in self.env.stateSpace:
-                y=0
-                for action in self.env.possibleActions:
-                    Qmatrix[x][y]=Q[state, action]
-                    y+=1
-                x +=1
+            y = 0
+            for action in self.env.possibleActions:
+                Qmatrix[x][y] = Q[state, action]
+                y += 1
+            x += 1
+
         np.savetxt(fileName, Qmatrix, delimiter=" ")
 
+    def loadQ(self, fileName: str) -> dict[tuple[Any, Any], Union[int, Any]]:   # TODO: forse dovrei capire meglio il tipo di dato
+        """
+        Carica in memoria la matrice Q ottenuta dalla fase di allenamento.
 
-    # Loading Q(state, action) matrix
-    def loadQ(self,fileName):
-        # inizializing Q(state, action) matrix to zero
+        :param fileName: nome del file che contiene la matrice
+        :return:
+        """
+
+        # initializing Q(state, action) matrix to zero
         Q = {}
         for state in self.env.stateSpace:
             for action in self.env.possibleActions:
                 Q[state, action] = 0
         with open(fileName) as file_name:
             Qmatrix = np.loadtxt(file_name, delimiter=" ")
-        #print(Qmatrix)
-        x=0
+
+        x = 0
         for state in self.env.stateSpace:
-                y=0
-                for action in self.env.possibleActions:
-                        Q[state,action]=Qmatrix[x][y]
-                        y+=1
-                x+=1
+            y = 0
+            for action in self.env.possibleActions:
+                    Q[state, action] = Qmatrix[x][y]
+                    y += 1
+            x += 1
+
         return Q
 
+    def execute(self, step_by_step=False, sleep_time=0.5):
+        """
+        Permette l'esecuzione del Q-Learning mostrando a schermo i movimenti dell'agente con il reward ottenuto.
+        Permette l'esecuzione step-by-step o automatica.
 
-    # EXECUTE
-    def execute(self, step_by_step=False):
+        :param step_by_step: se True, l'esecuzione viene fatta step-by-step, altrimenti automatica
+        :param sleep_time: tempo di attesa tra uno step e l'altro
+        """
+
+        # visualizzazione dello stato iniziale
         self.env.reset()
         self.env.render()
-        Q=self.loadQ("Qmatrix")
-        totReward=0
-        if step_by_step:
-            command=input("Return: "+str(totReward)+" ExecuteNext?(y/n/uP/dOWN/lEFT/riGTH):")
+
+        # carico la matrice Q
+        Q = self.loadQ("Qmatrix")
+
+        totReward = 0
+        if step_by_step:    # esecuzione step-by-step
+            command = input("Return: "+str(totReward)+" ExecuteNext?(y/n/uP/dOWN/lEFT/riGTH):")
             while  command != 'n':
-                if command =='y':
-                    action=self.maxAction(Q, self.env.state_to_int(self.env.get_state()) , self.env.possibleActions)
+                if command == 'y':
+                    action = self.maxAction(Q, self.env.state_to_int(self.env.get_state()) , self.env.possibleActions)
                 elif command == 'u':
-                    action='U'
+                    action = 'U'
                 elif command == 'd':
-                    action='D'
+                    action = 'D'
                 elif command == 'l':
-                    action='L'
+                    action = 'L'
                 elif command == 'r':
-                    action='R'
+                    action = 'R'
+
                 observationNext, reward, done, info = self.env.step(action)
                 totReward += reward
                 print("Action:"+str(action)+" Reward:"+str(reward)+"\n")
                 self.env.render()
-                command=input("Return: "+str(totReward)+" ExecuteNext?(y/n/uP/dOWN/lEFT/rIGTH):")
-        else:
+
+                command = input("Return: "+str(totReward)+" ExecuteNext?(y/n/uP/dOWN/lEFT/rIGTH):")
+
+        else:   # esecuzione automatica
             while True:
                 action = self.maxAction(Q, self.env.state_to_int(self.env.get_state()), self.env.possibleActions)
                 observationNext, reward, done, info = self.env.step(action)
                 totReward += reward
+
                 print("Action:" + str(action) + " Reward:" + str(reward) + "\n")
                 self.env.render()
 
@@ -97,21 +125,23 @@ class QLearning(object):
                     print("Return:" + str(totReward))
                     break
 
-                sleep(0.5)
+                sleep(sleep_time)      # tempo di attesa per visualizzare lo stato successivo
                 system("clear")
 
-    # TRAINING
     def training(self, epochs=50000, steps=200, ALPHA=0.1, GAMMA=1.0, EPS= 1.0, plot=True):
-        '''
-        default hyperparameters
-        ALPHA = 0.1 is the learning rate of q learning
-        GAMMA = 1.0 is the discount factor 
-        EPS = 1.0   is the epsilon and is related to the greedy value of the algorithm 
-        Epochs = 50000 is the max number of epochs
-        Steps = 200 is the max number of actions ( step ) per epoch
-    
-        '''
-    # inizializing Q(state, action) matrix to zero
+        """
+        Funzione che effettua il traning dell'agente con la possibilita' di modificare alcuni parametri.
+        Alla fine effettua il salvataggio dela Qmatrix in un file.
+
+        :param epochs: numero di epoche
+        :param steps: numero di step per ogni epoca (potrebbe essere il massimo numero di azioni)
+        :param ALPHA: learning rate
+        :param GAMMA: discount factor
+        :param EPS: epsilon-greedy
+        :param plot: stampa con matplotlib la learning curve
+        """
+
+        # inizializing Q(state, action) matrix to zero
         Q = {}
         for state in self.env.stateSpace:
             for action in self.env.possibleActions:
@@ -119,42 +149,37 @@ class QLearning(object):
 
         self.env.reset()
         self.env.render()
+
         totalRewards = np.zeros(epochs)
         for i in range(epochs):
             if i % int(epochs/10) == 0:
                 print('starting game ', i)
-                #self.env.render()
-                #pQ(Q,self.env)
+
             done = False
             epRewards = 0
-            numActions =0 
+            numActions = 0
             observation = self.env.reset()
-            while not done and numActions <= steps :
+            while not done and numActions <= steps:
                 rand = np.random.random()
-                action = self.maxAction(Q,observation, self.env.possibleActions) if rand < (1-EPS) \
+                action = self.maxAction(Q, observation, self.env.possibleActions) if rand < (1-EPS) \
                                                         else self.env.actionSpaceSample()
                 observationNext, reward, done, info = self.env.step(action)
-                numActions+= 1 
+                numActions += 1
                 epRewards += reward
-                # self.env.render()
+
                 actionNext = self.maxAction(Q, observationNext, self.env.possibleActions)
-                Q[observation,action] = Q[observation,action] + ALPHA*(reward + \
-                            GAMMA*Q[observationNext,actionNext] - Q[observation,action])
+                Q[observation, action] = Q[observation, action] + ALPHA*(reward + \
+                            GAMMA*Q[observationNext, actionNext] - Q[observation, action])
                 observation = observationNext
-            #self.env.render()
+
             if EPS - 2 / epochs > 0:
                 EPS -= 2 / epochs
             else:
                 EPS = 0
             totalRewards[i] = epRewards
-            #if epRewards > - 2000:
-                #self.env.render()
-                #print('Reward:',epRewards)
+
         if plot:
             plt.plot(totalRewards)
             plt.show()
-            #self.printQ(Q)
 
-        self.saveQ(Q,"Qmatrix")
-    #   saveQ2(Q,env,"Qmatrix2")
-    #   saveQ(Q, "Qmatrix")
+        self.saveQ(Q, "Qmatrix")
