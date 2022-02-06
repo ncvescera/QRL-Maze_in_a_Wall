6feb0678eff1b8b5249f6d7ea6cb213614b9c41f
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-02-06 10:36:55 +0100

    Commentato generate_dataset.py

--- mazeinawall/generate_dataset.py
+++ mazeinawall/generate_dataset.py
@@ -9,14 +9,22 @@ training_folder = "training"
 testing_folder = "testing"
 
 tot_elem_training = 100  # numero di matrici da generare
-tot_elem_testing = 10
-max_w = 10  # massima altezza
-max_h = 10  # massima lunghezza
-min_w = 3
-min_h = 3
+tot_elem_testing = 10    # numero di matrici da generare
+max_w = 10               # massima altezza
+max_h = 10               # massima lunghezza
+min_w = 3                # minima altezza
+min_h = 3                # minima larghezza
 
 
 def generate_dataset():
+    """
+    Genera il dataset di training e testing creando matrici a caso
+    di dimensione massima 10x10, minima 3x3 e con un numero minimo di 1 muro
+
+    :return:
+    """
+
+    # imposto il seed
     np.random.seed(seed_number)
     seed(seed_number)
 
@@ -25,12 +33,21 @@ def generate_dataset():
 
 
 def generate_testing(dim: int):
+    """
+    Genera il dataset di testing.
+    Se la cartella non esiste la crea e la popola con matrici a caso.
+
+    :param dim: numero di matrici da creare
+    :return:
+    """
+    # se la cartella non esiste la creo
     if not path.exists(testing_folder):
         mkdir(testing_folder)
 
     for elem in range(dim):
         file_name = f"{testing_folder}/matrice_{elem}"
 
+        # scelta random di w, h e walls
         w = randint(min_w, max_w)
         h = randint(min_h, max_h)
         walls = randint(1, int(w * h / 2) - 1)
@@ -41,12 +58,22 @@ def generate_testing(dim: int):
 
 
 def generate_training(dim: int):
+    """
+    Genera il dataset di training.
+    Se la cartella non esiste la crea e la popola con matrici a caso.
+
+    :param dim: numero di matrici da creare
+    :return:
+    """
+
+    # se la cartella non esiste la creo
     if not path.exists(training_folder):
         mkdir(training_folder)
 
     for elem in range(dim):
-        file_name = f"{training_folder}/matrice_{elem}"
+        file_name = f"{training_folder}/matrice_{elem}"\
 
+        # scelta random di w, h e walls
         w = randint(min_w, max_w)
         h = randint(min_h, max_h)
         walls = randint(1, int(w * h / 2) - 1)
