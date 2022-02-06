6bb8815c9c618be0c767715e393c5afd3d637925
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-02-06 10:30:23 +0100

    Commentati training.py e testing.py

--- mazeinawall/testing.py
+++ mazeinawall/testing.py
@@ -4,14 +4,27 @@ from maze_utils import grid_from_file, maze_filename
 
 
 def testing():
+    """
+    Avvia la fase di testing/execution.
+
+    Permette di:
+     - scegliere all'utente se la fase di esecuzione deve essere fatta step-by-step oppure automatica
+     - se l'output verra' stampato su terminale o su una finestra apposita (GUI).
+
+    :return:
+    """
+
     print("Execute\n")
 
+    # carica labirinto da file
     grid, message = grid_from_file(maze_filename)
 
+    # controlla se e' stato caricato con successo
     if grid is None:
         print(message)
         return
 
+    # creo l'ambiente e l'algoritmo per il qlearning
     env = GridWorld(grid=grid)
     QL = QLearning(env)
 
@@ -19,9 +32,11 @@ def testing():
     scelta = input("Esecuzione step-by-step? (y/N): ").lower()
     step_by_step = True if scelta == 'y' else False
 
+    # scelta GUI
     scelta = input("GUI? (Y/n): ").lower()
     gui = False if scelta == 'n' else True
 
+    # avvio la fase di esecuzione
     QL.execute(step_by_step=step_by_step, gui=gui)
 
 
--- mazeinawall/training.py
+++ mazeinawall/training.py
@@ -4,6 +4,7 @@ from maze_utils import grid_from_file, maze_filename
 from os import walk
 
 
+# dataset path
 dataset = "training"
 
 # training values
@@ -15,36 +16,56 @@ EPS = 0.9
 
 
 def training():
+    """
+    Avvia la fase di training/apprendimento.
+
+    Permette all'utente di:
+     - scegliere se riprendere o meno un allenamento precedente (ricaricare la matrice Q e modificarla)
+     - Caricare un labirinto esistente oppure utilizzare le metrici nella cartella "training"
+
+    :return:
+    """
     grid = None
     env = None
     QL = None
 
-    print("Training Script")
+    print("Training\n")
 
+    # scelta per modifica della matrice Q esistente
     scelta = input("Riprendere allenamento ? (Y/n)").lower()
     resume = True if scelta != 'n' else False
 
+    # scelta per utilizzo di singola matrice o dataset
     scelta = input("Caricare matrice gia' esistente ? (Y/n)").lower()
     if scelta != 'n':
+        print(f"Avvio training sul labirinto esistente")
+
         grid, message = grid_from_file(maze_filename)
 
         if grid is None:
             print(message)
             return
 
+        print(f"Caricato labirinto {grid.shape}")
+
         env = GridWorld(grid=grid)
         QL = QLearning(env)
         QL.training(epochs=epochs, steps=steps, ALPHA=ALPHA, GAMMA=GAMMA, EPS=EPS, plot=True, resume=resume)
 
     else:
-        filenames = next(walk(dataset), (None, None, []))[2]
+        print(f"Avvio training sul dataset: {dataset}")
+
         QL = QLearning(None)
 
+        # ottengo tutti i file nella cartella del dataset
+        filenames = next(walk(dataset), (None, None, []))[2]
         for file in filenames:
             grid, message = grid_from_file(f"{dataset}/{file}")
 
             if grid is None:
                 print(f"Errore in {file}: {message}")
+
+            print(f"Caricato labirinto {grid.shape}")
 
             env = GridWorld(grid=grid)
             QL.env = env
